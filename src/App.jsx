2a905bb9309674d5842c40cd005cf722d338fcf2
Brian Sanchez <briansanchez@Brians-MacBook-Pro.local>
2023-09-01 14:13:58 -0500

    app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+/* eslint-disable */
 // Write your Color component here
+import { useState } from 'react'
 
+
+
+const Color = (props) => {
+  return <div className={props.color+(props.color===props.selectedColor? " selected": "")}
+  onClick={() => props.setSelectedColor(props.color)}>
+
+  </div>
+}
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="green" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="blue" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+      </div>
     </div>
   );
 };
